ef10593399ed34d2dcdb7be7a58455f6063b3a73
Karol Kucharski <187746@edu.p.lodz.pl>
2016-10-11 22:32:00 +0200

    Przykladowi uzytkownicy.

--- src/main/java/pl/iis/paw/trello/TrelloApplication.java
+++ src/main/java/pl/iis/paw/trello/TrelloApplication.java
@@ -1,7 +1,11 @@
 package pl.iis.paw.trello;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.iis.paw.trello.domain.User;
+import pl.iis.paw.trello.repository.UserRepository;
 
 @SpringBootApplication
 public class TrelloApplication {
@@ -9,4 +13,15 @@ public class TrelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrelloApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner demoUsers(UserRepository userRepository) {
+		return (args) -> {
+			userRepository.save(new User("aaa", "ap", "a@x.com"));
+			userRepository.save(new User("bbb", "bp", "b@x.com"));
+			userRepository.save(new User("ccc", "cp", "c@x.com"));
+			userRepository.save(new User("ddd", "dp", "d@x.com"));
+		};
+	}
+	
 }
