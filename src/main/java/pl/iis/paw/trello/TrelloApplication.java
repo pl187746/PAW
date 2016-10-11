b49ebe5cc20570b37e952b4450f32688d6e616ad
Karol Kucharski <187746@edu.p.lodz.pl>
2016-10-11 22:41:39 +0200

    Przykladowe tablice.

--- src/main/java/pl/iis/paw/trello/TrelloApplication.java
+++ src/main/java/pl/iis/paw/trello/TrelloApplication.java
@@ -1,10 +1,17 @@
 package pl.iis.paw.trello;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import pl.iis.paw.trello.domain.Board;
 import pl.iis.paw.trello.domain.User;
+import pl.iis.paw.trello.repository.BoardRepository;
+import pl.iis.paw.trello.repository.CardListRepository;
+import pl.iis.paw.trello.repository.CardRepository;
 import pl.iis.paw.trello.repository.UserRepository;
 
 @SpringBootApplication
@@ -21,6 +28,15 @@ public class TrelloApplication {
 			userRepository.save(new User("bbb", "bp", "b@x.com"));
 			userRepository.save(new User("ccc", "cp", "c@x.com"));
 			userRepository.save(new User("ddd", "dp", "d@x.com"));
+		};
+	}
+	
+	@Bean
+	public CommandLineRunner demoBoards(BoardRepository boardRepository) {
+		return (args) -> {
+			boardRepository.save(new Board("Tabliczka 1", null));
+			boardRepository.save(new Board("Tabliczka 2", null));
+			boardRepository.save(new Board("Tabliczka 3", null));
 		};
 	}
 	
