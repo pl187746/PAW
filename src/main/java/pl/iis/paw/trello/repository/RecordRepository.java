6c93254ccdf9ed5db66864219d988dfc3db6ca10
Karol Kucharski <187746@edu.p.lodz.pl>
2016-11-04 16:51:14 +0100

    Wyszukiwanie wpisow dziennika na podstawie boardId i daty.

--- src/main/java/pl/iis/paw/trello/repository/RecordRepository.java
+++ src/main/java/pl/iis/paw/trello/repository/RecordRepository.java
@@ -1,11 +1,22 @@
 package pl.iis.paw.trello.repository;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.TemporalType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Temporal;
 import org.springframework.stereotype.Repository;
 
+import pl.iis.paw.trello.domain.Board;
 import pl.iis.paw.trello.domain.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+	
+	public List<Record> findByBoard(Board board);
 
+	public List<Record> findByBoardAndTimestampAfter(Board board, @Temporal(TemporalType.TIMESTAMP) Date timestamp);
+	
 }
