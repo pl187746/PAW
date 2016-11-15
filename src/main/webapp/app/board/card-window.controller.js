7a49138a11bb6ba235e4fbe26bd5a7b7ad956d91
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-11-15 15:37:19 +0100

    modyfikacja i usuwanie komentarzy

--- src/main/webapp/app/board/card-window.controller.js
+++ src/main/webapp/app/board/card-window.controller.js
@@ -4,9 +4,9 @@ angular
     .module('trello')
     .controller('CardWindowController', CardWindowController);
 
-CardWindowController.$inject = ['$scope', '$uibModalInstance', 'entity', 'Comment'];
+CardWindowController.$inject = ['$scope', '$uibModalInstance','Subject', 'entity', 'Comment'];
 
-function CardWindowController ($scope, $uibModalInstance, entity, Comment) {
+function CardWindowController ($scope, $uibModalInstance, Subject, entity, Comment) {
     const VIEWS = {
         COMMENTS: 'COMMENTS',
         ATTACHMENTS: 'ATTACHMENTS'
@@ -15,10 +15,24 @@ function CardWindowController ($scope, $uibModalInstance, entity, Comment) {
     $scope.close = close;
     $scope.changeView = changeView;
     $scope.addComment = addComment;
+    $scope.updateComment = updateComment;
+    $scope.removeComment = removeComment;
 
     $scope.VIEWS = VIEWS;
     $scope.card = entity;
     $scope.commentContent = '';
+    $scope.user = null;
+
+
+    getAccount();
+
+    function getAccount() {
+        Subject.identity().then(function(account) {
+            if (account !== null) {
+            }
+            $scope.user = account;
+        });
+    }
 
     changeView(VIEWS.COMMENTS);
 
@@ -45,6 +59,35 @@ function CardWindowController ($scope, $uibModalInstance, entity, Comment) {
 
         function onError() {
             console.log('Error while adding comment')
+        }
+    }
+
+    function updateComment(comment) {
+        console.log('Update comment request for card with id: ' + $scope.card.id + ' and content: ' + comment);
+
+        Comment.update(comment,onSuccess,onError);
+
+        function onSuccess(response) {
+            console.log('Updated comment in card with id ' + $scope.card.id);
+        }
+
+        function onError() {
+            console.log('Error while updating comment');
+        }
+    }
+
+    function removeComment(comment, card) {
+        console.log('Delete comment request for card with id: ' + $scope.card.id + ' and content: ' + comment);
+        var commentIndex = $scope.card.comments.indexOf(comment);
+        $scope.card.comments.splice(commentIndex,1);
+
+        Comment.delete({id: comment.id},onSuccess,onError);
+        function onSuccess(response) {
+            console.log('Deleted comment in card with id ' + $scope.card.id);
+        }
+
+        function onError() {
+            console.log('Error while deleting comment');
         }
     }
 
