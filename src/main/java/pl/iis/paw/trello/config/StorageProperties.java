1b89329158b1762fdb9b5f636b69cb2e2ff9b50d
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-15 15:00:54 +0100

    konfiguracja sciezki w application.properties

--- src/main/java/pl/iis/paw/trello/config/StorageProperties.java
+++ src/main/java/pl/iis/paw/trello/config/StorageProperties.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class StorageProperties {
 
-    @Value("${storage.images.location}")
+    @Value("${storage.attachments.location}")
     private String location;
 
     public StorageProperties() { }
