a89f8fec3885f00286bee69b91173b7fb46df9fd
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-15 14:59:19 +0100

    implementacja storage service (system plikow)

--- src/main/java/pl/iis/paw/trello/exception/StorageException.java
+++ src/main/java/pl/iis/paw/trello/exception/StorageException.java
@@ -1,10 +1,16 @@
 package pl.iis.paw.trello.exception;
 
+import java.io.IOException;
+
 public class StorageException extends RuntimeException {
 
     public StorageException() { }
 
     public StorageException(String message) {
         super(message);
+    }
+
+    public StorageException(String message, IOException e) {
+        super(message, e);
     }
 }
