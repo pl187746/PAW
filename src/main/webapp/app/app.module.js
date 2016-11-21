5a9f59b2c8d54d7bcf7780973c710fd94e59693e
187796 <187796@edu.p.lodz.pl>
2016-11-21 14:45:42 +0100

    dodanie zmiany jezyka

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('trello', ['ngResource', 'ui.router', 'ui.bootstrap', 'ngFileUpload'])
+        .module('trello', ['ngResource', 'ui.router', 'ui.bootstrap', 'ngFileUpload','pascalprecht.translate'])
         .run(run);
 
     run.$inject = ['stateHandler'];
