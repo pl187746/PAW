bb2e969ce5583222f945cfaa05c430dc67886277
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-29 18:45:31 +0100

    dodanie biblioteki wspierajacej drag & drop

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('trello', ['ngResource', 'ui.router', 'ui.bootstrap', 'ngFileUpload','pascalprecht.translate', 'ui.bootstrap.datetimepicker'])
+        .module('trello', ['ngResource', 'ui.router', 'ui.bootstrap', 'ngFileUpload','pascalprecht.translate', 'ui.bootstrap.datetimepicker', 'dndLists'])
         .run(run);
 
     run.$inject = ['stateHandler'];
